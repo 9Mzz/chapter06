bfa8f82be9bef6a3469f7550159390a574ad4a58
구민석 <ser_om@naver.com>
2022-03-13 03:07:23 +0900

    DigitRunableImp6

--- src/echo/ex06/MultiThread.java
+++ src/echo/ex06/MultiThread.java
@@ -1,25 +1,24 @@
 package echo.ex06;
 
-import echo.ex05.DigitRunableimpl;
+import echo.ex05.DigitRunableImpl;
 
 public class MultiThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		Runnable digit = new DigitRunableimpl();
+		Runnable digit = new DigitRunableImpl();
 		Runnable lower = new LowerThread();
 		Runnable upper = new UpperThread();
-				
+
 		Thread thread1 = new Thread(digit);
 		Thread thread2 = new Thread(lower);
 		Thread thread3 = new Thread(upper);
-		
 
 		thread1.start();
 		thread2.start();
 		thread3.start();
 		for (char ch = 'Z'; ch <= 'Z'; ch++)
-			System.out.println(ch+"========================");
+			System.out.println(ch + "========================");
 		Thread.sleep(1000);
 	}
 
